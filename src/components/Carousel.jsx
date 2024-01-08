a082bae3cd3fad57d18fef318dc8d4bb01f3acbc
Ram nathawat <nathawatr7@gmail.com>
2024-01-08 16:57:27 +0530

    final

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,41 @@
 import React, { useState } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
-
-// you can research - how to use material ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// complete the function below:
 function Carousel() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(
+      (prevSlide) => (prevSlide - 1 + images.length) % images.length
+    );
+  };
+
+  return (
+    <div className="carousel-container">
+      <img
+        src={images[currentSlide].img}
+        alt={images[currentSlide].title}
+        className="carousel-img"
+      />
+      <button className="carousel-buttons left" onClick={prevSlide}>
+        <ArrowBackIosIcon />
+      </button>
+      <button className="carousel-buttons right" onClick={nextSlide}>
+        <ArrowForwardIosIcon />
+      </button>
+      <div className="carousel-text">
+        <h1>{images[currentSlide].title}</h1>
+        <p>{images[currentSlide].subtitle}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Carousel;
